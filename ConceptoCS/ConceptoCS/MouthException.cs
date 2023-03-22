48ed332d50d8cfaf03e13c7e0d702ebad0cf107d
CesRom04 <ces.romhdz@gmail.com>
2023-03-22 10:44:26 -0600

    Finish first video class

---
+++ ConceptoCS/ConceptoCS/MouthException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptoCS
+{
+    [Serializable]
+    internal class MouthException : Exception
+    {
+        public MouthException()
+        {
+        }
+
+        public MouthException(string? message) : base(message)
+        {
+        }
+
+        public MouthException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected MouthException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ ConceptoCS/ConceptoCS/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptoCS;
+
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+	Console.WriteLine(new Date(2023, 2, 29));
+	Console.WriteLine(new Date(2023, 4, 2));
+	Console.WriteLine(new Date(2023, 12, 32));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
